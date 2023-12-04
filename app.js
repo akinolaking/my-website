ed5c3b47eeff2d9b47fb296b3f7be8e30830a344
Muyiwa Akinola <fadga2011@gmail.com>
2023-12-04 05:59:53 +0000

    Before Boolean Lesson

---
+++ app.js
@@ -0,0 +1,21 @@
+
+///////Ecercise for declaring Variables and assigning Values to Variables. let, const and var can be used to declare variables. A value of a variable declared with const (constant), can not be changed while let and var values can be changed ////////
+// let name = "Akinola";
+// let whatDoYouWannaBecomeInLife = "Programmer";
+// let gender = "Male";
+// let twitterHandle ="@akinolaking"
+
+// console.log(name, whatDoYouWannaBecomeInLife, gender, twitterHandle);
+
+
+//////////// Excercise for Primitive Number and Methods  /////
+// let firstFavNum = 33;
+// let secondFavNum = 22;
+
+// console.log(firstFavNum + secondFavNum);
+// console.log(firstFavNum - secondFavNum);
+// console.log(firstFavNum * secondFavNum);
+// console.log(firstFavNum / secondFavNum);
+// console.log(firstFavNum % secondFavNum);
+// console.log(firstFavNum ** secondFavNum);
+
